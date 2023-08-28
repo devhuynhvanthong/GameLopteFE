313b30f35deed8979ba0074ce04028241429e41e
Dev.Huỳnh Thông <dev.huynhvanthong@gmail.com>
2023-08-28 23:54:34 +0700

    Merge branch 'test' into dev
    # Conflicts:
    #	src/utils/Urls.js

--- src/utils/Cookies.js
+++ src/utils/Cookies.js
@@ -10,7 +10,7 @@ export default function Cookies() {
             expires: 30,
             secure: true,
             sameSite: 'strict',
-            domain: 'aigoox.com'
+            domain: 'aigoox.com',
         })
     }
 
